05c5fda44649bb36d8b5f0ef65228a899e02c881
Cheyroseflammer <cheyroseflammer@gmail.com>
2020-12-03 11:04:10 -0800

    header icons

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  API_ENDPOINT: "http://localhost:9000/api",
+  API_ENDPOINT:
+    // "https://git.heroku.com/shielded-dusk-25165.git/api" ||
+    "http://localhost:9000/api",
   API_TOKEN: process.env.API_TOKEN,
 };
