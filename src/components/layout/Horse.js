bd8909631ad0d4f47a6cff4622d7221691e2b0cb
Chey Rose Flammer <cheyroseflammer@gmail.com>
2021-01-09 10:04:35 -0800

    start Horse

--- src/components/layout/Horse.js
+++ src/components/layout/Horse.js
@@ -1,10 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ApiContext from "../../ApiContext";
-import config from "../../config";
-import "../../styles/Horse.css";
-// import { InlineIcon } from "@iconify/react";
-// import trashCan from "@iconify-icons/mdi/trash-can";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ApiContext from '../../ApiContext';
+import config from '../../config';
+import '../../styles/Horse.css';
 
 export default class Horse extends React.Component {
   static defaultProps = {
@@ -17,7 +15,7 @@ export default class Horse extends React.Component {
     const horseId = this.props.id;
 
     fetch(`${config.API_ENDPOINT}/horses/${horseId}`, {
-      method: "DELETE",
+      method: 'DELETE',
     })
       .then((res) => {
         if (!res.ok) return res.json().then((e) => Promise.reject(e));
